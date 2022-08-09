a922e7b07968fa25dbfd8b8f7d3b6781bf8be3cb
steveoh <sgourley@utah.gov>
2022-08-08 18:09:19 -0600

    feat: :star:

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+const http = require('@actions/http-client');
+const auth = require('@actions/http-client/lib/auth');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const token = core.getInput('repoToken', { required: true });
+    const username = core.getInput('username', { required: true });
+    const password = core.getInput('password', { required: true });
+    const systemId = core.getInput('system-id', { required: true });
+    const tableName = core.getInput('table-name', { required: false });
+    const instanceName = core.getInput('instance-name', { required: false });
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.setSecret(token);
+    core.setSecret(username);
+    core.setSecret(password);
+    core.setSecret(systemId);
+    core.setSecret(tableName);
+    core.setSecret(instanceName);
 
-    core.setOutput('time', new Date().toTimeString());
+    const httpClient = new http.HttpClient(
+      'service-now-work-notes-github-action',
+      [new auth.BasicCredentialHandler(username, password)]
+    );
+
+    const url = `https://${instanceName}.service-now.com/api/now/table/${tableName}/${systemId}`;
+
+    const octokit = github.getOctokit(token);
+    const owner = core.getInput('repositoryOwner');
+    const repo = core.getInput('repository');
+    const runId = github.context.runNumber;
+
+    core.info(`Run ID: ${runId}`);
+
+    const { data } = await octokit.request(
+      `GET /repos/${owner}/${repo}/actions/runs/${runId}/approvals`,
+      {
+        owner,
+        repo,
+        run_id: runId,
+      }
+    );
+
+    core.info(`Approvals: ${JSON.stringify(data)}`);
+    const lastAttempt = data[0];
+
+    core.info(lastAttempt.user.login, lastAttempt.comment);
+
+    const approver = lastAttempt.user.login;
+    const comments = lastAttempt.comment;
+
+    let notes = `This item has been deployed and approved by ${approver}.`;
+    if (comments.length > 0) {
+      notes += ` The following comments were added to the approval: ${comments}`;
+    }
+
+    const response = httpClient.patchJson(url, { work_notes: notes });
+    if (response.statusCode != 200) {
+      core.setFailed(
+        'request failed:' + response.statusCode + ',' + response.result
+      );
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
